7d4f7a9c12fce41273f73865fd58ac63fde6f828
rushdi21-meet <rushdi21@meet.mit.edu>
2019-07-23 05:38:16 -0400

    Stopping here

---
+++ AgeCompareCalculator.py
@@ -0,0 +1,4 @@
+person_one = int(input("What is the age of the younger person?"))
+person_two = int(input("What is the age of the older person? "))
+age_difference = person_two - person_one
+print(age_difference)
